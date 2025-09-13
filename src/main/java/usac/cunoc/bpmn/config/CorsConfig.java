0900eb1b87c5f93e29d54238ed3b566d2d3b266e
Diego Maldonado <dj_maldonado19@hotmail.es>
2025-09-12 23:55:01 -0600

    Fix: adding cors from anywhere

--- src/main/java/usac/cunoc/bpmn/config/CorsConfig.java
+++ src/main/java/usac/cunoc/bpmn/config/CorsConfig.java
@@ -20,10 +20,7 @@ public class CorsConfig {
 
         // Allow Nuxt.js and other development origins
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "http://127.0.0.1:3000",
-                "http://localhost:5173",
-                "http://127.0.0.1:5173"));
+                "*"));
 
         // Allow all common HTTP methods
         configuration.setAllowedMethods(Arrays.asList(
